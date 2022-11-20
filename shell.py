c4259f7fc4c007b3a96e1d4f3289670f8f5d4690
James <phamthach178@gmail.com>
2022-11-19 23:58:02 -0500

    Push more code

--- shell.py
+++ shell.py
@@ -1,8 +1,14 @@
 import hello
 
 while True:
-    text = input('hello > ')
-    result, error = hello.run('<stdin>', text)
+	text = input('hello > ')
+	if text.strip() == "": continue
+	result, error = hello.run('<stdin>', text)
 
-    if error: print(error.as_string())
-    else: print(result)
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
